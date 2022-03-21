9bd7b4c84f66c63e0eb2527102bc459e0cc7eb4b
Ceyda <ceydaonemli2@gmail.com>
2022-03-21 15:10:51 +0300

    Sidebar logo and navigation completed

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { HomeIcon } from '../icons/Icon';
+import twitterLogo from "../images/twitter.svg"
+import { HomeIcon, LikeIcon } from '../icons/Icon';
 import { ExploreIcon } from '../icons/Icon';
 import { NotificationsIcon } from '../icons/Icon';
 import { MessagesIcon } from '../icons/Icon';
@@ -7,20 +8,68 @@ import { BookmarksIcon } from '../icons/Icon';
 import { ListsIcon } from '../icons/Icon';
 import { ProfileIcon } from '../icons/Icon';
 import { MoreIcon } from '../icons/Icon';
+import SideLink from '../components/SideLink';
+
+
+const sideLinks = [
+    {
+        name: "Home",
+        icon: HomeIcon,
+    },
+    {
+        name: "Explore",
+        icon: ExploreIcon,
+    },
+    {
+        name: "Notifications",
+        icon: NotificationsIcon,
+    },
+    {
+        name: "Messages",
+        icon: MessagesIcon,
+    },
+    {
+        name: "Bookmarks",
+        icon: BookmarksIcon,
+    },
+    {
+        name: "Lists",
+        icon: ListsIcon,
+    },
+    {
+        name: "Profile",
+        icon: ProfileIcon,
+    },
+    {
+        name: "More",
+        icon: MoreIcon,
+    },
+];
 
 const Sidebar = () => {
     return (
-        <div className='w-72 bg-blue-200' >
-            <h1>Sidebar</h1>
-            <HomeIcon />
-            <ExploreIcon />
-            <NotificationsIcon />
-            <MessagesIcon />
-            <BookmarksIcon />
-            <ListsIcon />
-            <ProfileIcon />
-            <MoreIcon />
-            
+        <div className='flex flex-col justify-between w-72 px-2' >
+            <div>
+                <div className='mt-1 mb-4 ml-1 flex items-center justify-center w-12 h-12 rounded-full hover:bg-gray-lightest transition-colors duration-200'>
+                    <img src={twitterLogo} alt="Twitter Logo " classname="w-9 h-9" />
+                </div>
+                <nav className='mb-4'>
+                    <ul>
+                        {sideLinks.map(({ name, icon }) => (
+                            <SideLink
+                                key={name}
+                                name={name}
+                                Icon={icon}
+                            />
+                        ))}
+                    </ul>
+                </nav>
+                <button className="bg-primary-base hover:bg-primary-dark text-white shadow-lg rounded-full py-3 px-8 w-11/12 transform transition-colors duration-200">
+                    Tweet
+                </button>
+            </div>
+            <div>Alt Taraf</div>
+
         </div>
     )
 }
